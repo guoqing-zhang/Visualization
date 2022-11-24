d02e2c6be7b77dd5eed82348870f09d77fc10738
zhangguoqing <zhangguoqing84@westlake.edu.cn>
2022-11-24 14:18:28 +0800

    no message

--- patchwork/patchwork.py
+++ patchwork/patchwork.py
@@ -11,6 +11,8 @@ Created on Thu Nov 24 10:18:56 2022
 import patchworklib as pw
 import matplotlib.pyplot as plt
 
+
+# Maplotlib，使用pw.Brick方法
 plt.style.use("ggplot")
 
 ax1 = pw.Brick(figsize=(1,2)) #每个子图调用pw.Brick方法
@@ -27,3 +29,29 @@ ax4.scatter(range(5), range(5))
 
 ax1234 = (ax1 | ax2) | (ax3 / ax4)
 ax1234.savefig()
+
+
+#>------------------------------------------------
+# Seaborn, 使用pw.Brick方法 (Axes水平)
+import pandas as pd
+import seaborn as sns
+import patchworklib as pw
+
+# ax1
+ax1 = pw.Brick(figsize=(3,2))
+fmri = sns.load_dataset("fmri")
+sns.lineplot(x = "timepoint", y="signal", hue="region", 
+             style="event",data = fmri, ax = ax1)
+ax1.move_legend(new_loc="upper left", bbox_to_anchor=(1.05,1.0))
+ax1.set_title("ax1")
+
+# ax2
+ax2 = pw.Brick(figsize=(1,2))
+titanic = sns.load_dataset("titanic")
+sns.barplot(x = "sex",y = "survived",hue="class",data=titanic,ax=ax2)
+ax2.move_legend(new_loc="upper left", bbox_to_anchor=(1.05,1.0))
+ax2.set_title("ax2")
+
+
+
+ax12345.savefig()
